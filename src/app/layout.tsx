1c78e3f990647cca8fcdc29387cfcd79ebd84197
G1anpierre <fgianpierre@gmail.com>
2024-01-19 17:54:09 +0100

    first implementation of Navbar mobile and desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import {Providers} from './providers'
+import {Nav} from '@/components/Nav'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -13,8 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="light">
-      <body className={`${inter.className} container px-3 m-auto`}>
-        <Providers>{children}</Providers>
+      <body className={`${inter.className} `}>
+        <Providers>
+          <Nav />
+          {children}
+        </Providers>
       </body>
     </html>
   )
