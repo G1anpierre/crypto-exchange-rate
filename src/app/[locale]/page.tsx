24d18c3ec579ae74b9c292d2630fc55bdf2df26b
G1anpierre <fgianpierre@gmail.com>
2024-11-01 10:09:04 +0100

    feat: use turbo for development

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,5 @@
 import {CryptoChart} from '@/components/CryptoChart'
 import {Hero} from '@/components/Hero'
-import {headers} from 'next/headers'
 import {StadisticChart} from '@/components/StadisticChart'
 import {getTranslations} from 'next-intl/server'
 
@@ -17,10 +16,6 @@ export default async function Home() {
     ],
     queryFn: () => cryptoStadistics('EUR', 'BTC', 'DIGITAL_CURRENCY_MONTHLY'),
   })
-
-  const headerValues = headers()
-
-  console.log({headerValues})
 
   const t = await getTranslations('Stadistics')
   return (
